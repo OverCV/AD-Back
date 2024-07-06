411e229e0b8c9ed8a6662db4027075c11b3da2a4
OH <over.castrillon22396@ucaldas.edu.co>
2024-07-06 01:35:46 -0500

    binary product is possible.

--- product.py
+++ product.py
@@ -15,6 +15,8 @@ def binary_product(
     # Flatten arrays if they're 2D with only one row
     u = u.flatten()
     v = v.flatten()
+    print(u_idx, v_idx)
+    print(u, v)
 
     d_len = len(u_idx) + len(v_idx)
 
@@ -22,14 +24,29 @@ def binary_product(
     endian_keys = lil_endian(d_len) if le else big_endian(d_len)
     df_result = pd.DataFrame([result], columns=endian_keys)
 
-    for key in endian_keys:
-        u_key = ''.join(key[i] for i in u_idx)
-        v_key = ''.join(key[i] for i in v_idx)
-        u_val = u[int(u_key, 2)]
-        v_val = v[int(v_key, 2)]
-        df_result.at[0, key] = u_val * v_val
-
     # Create the union of indices and sort
     combined_idx = tuple(sorted(set(u_idx) | set(v_idx)))
+    print(combined_idx)
 
+    for key in endian_keys:
+        u_key = ''.join(key[combined_idx.index(i)] for i in u_idx)
+        v_key = ''.join(key[combined_idx.index(i)] for i in v_idx)
+        u_val = u[int(u_key[::-1], 2)]
+        v_val = v[int(v_key[::-1], 2)]
+        print(f'u_val <- (u[{u_key}] = {u_val})')
+        print(f'v_val <- (v[{v_key}] = {v_val})')
+        print(f'{key} <- ({u_val} * {v_val} = {u_val * v_val})')
+        df_result.at[0, key] = u_val * v_val
+
+    print(df_result)
+    print()
     return combined_idx, df_result.values
+
+
+A = (0,), np.array([1, 0], dtype=np.float64)
+B = (1,), np.array([0.75, 0.25], dtype=np.float64)
+C = (2,), np.array([0, 1], dtype=np.float64)
+
+AC = binary_product(A, C)
+
+ABC = binary_product(AC, B)
