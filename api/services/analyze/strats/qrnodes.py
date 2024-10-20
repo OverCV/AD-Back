e004cd6ff0ba6a33cf45f2f16d9bd2d130450cf7
OH <over.castrillon22396@ucaldas.edu.co>
2024-10-20 11:42:54 -0500

    Creating branch for testing.

--- api/services/analyze/strats/qrnodes.py
+++ api/services/analyze/strats/qrnodes.py
@@ -184,7 +184,7 @@ class QRNodes(Sia):
                     u_hist = u_dist.create_distrib(u_effect, u_actual, data=True)
                     u_dist = u_hist[StructProps.DIST_ARRAY]
                     u_emd = emd_pyphi(*u_dist, *self._target_dist)
-                    print(f'{u_emd=:.2f}', u_dist)
+                    # print(f'{u_emd=:.2f}', u_dist)
 
                     # Creamos el objeto Deletion con la información
                     current_mip = Deletion(
@@ -197,7 +197,7 @@ class QRNodes(Sia):
                     )
                     all_mips.append(current_mip)
 
-                    print(f'{o_emd:.2f} - {u_emd:.2f} = {o_emd - u_emd:.2f}')
+                    # print(f'{o_emd:.2f} - {u_emd:.2f} = {o_emd - u_emd:.2f}')
                     print(SEP3)
 
                 # Seleccionamos el mejor MIP de esta iteración
@@ -227,116 +227,6 @@ class QRNodes(Sia):
     #     o_concept = act{False: [b], True: [ca]}, eff{False: [C], True: [AB]}
     #     o_actual  = {False: [1], True: [2,0]}
     #     o_effect  = {False: [2], True: [0,1]}
-
-    #     # Sólo tomamos el nuevo U .
-
-    #     UR2 = {C}                  COM UR2 = {abcBA}
-
-    #     u_concept = act{False: [],   True: [abc]}, eff{False: [BA], True: [C]}
-    #     u_actual  = {False: [0,1,2], True: []}
-    #     u_effect  = {False: [1,0],   True: [2]}
-
-    #     Mirar conjunto, uno lo pondrá en el true y el otro en el false.
-    #     """
-
-    #     # struct: Structure = copy.deepcopy(self._structure)
-    #     actual = [(INT_ZERO, i) for i in self._actual]
-    #     effect = [(INT_ONE, j) for j in self._effect]
-    #     ic(actual, effect)
-
-    #     rep_one, rep_two = BOOL_RANGE
-
-    #     alpha: set[tuple[int, int]] = set(actual + effect)
-    #     omega: set[int] = set()
-
-    #     for t in alpha:
-    #         # all base elements
-    #         t_com = alpha - {t}
-
-    #         omega = set((t,))
-
-    #         ic(t, t_com)
-
-    #         while len(omega) < len(alpha):
-    #             all_mips: list[Deletion] = []
-
-    #             for u in t_com:
-    #                 omega_dist: Structure = copy.deepcopy(self._structure)
-    #                 u_dist: Structure = copy.deepcopy(self._structure)
-
-    #                 betha = omega.copy()
-    #                 betha.add(u)
-
-    #                 # Ciclos asociados u
-
-    #                 u_com = alpha - {u}
-
-    #                 idx_u_actual = {bin: [] for bin in BOOL_RANGE}
-    #                 idx_u_effect = {bin: [] for bin in BOOL_RANGE}
-    #                 u_concept = (idx_u_actual, idx_u_effect)
-    #                 print('-' * 20)
-
-    #                 ic(u)
-    #                 ic(u_com)
-
-    #                 for p, q in [u]:
-    #                     u_concept[1 - p][rep_one].append(q)
-    #                     # print(p, u_concept[p], 1 - p, u_concept[1 - p])
-
-    #                 for i, j in u_com:
-    #                     u_concept[1 - i][rep_two].append(j)
-    #                     # print(p, u_concept[p], 1 - p, u_concept[1 - p])
-
-    #                 u_effect, u_actual = u_concept
-    #                 ic(u_effect)
-    #                 ic(u_actual)
-
-    #                 # Creación de distribuciones en u
-    #                 u_hist = u_dist.create_distrib(u_effect, u_actual, data=True)
-    #                 u_dist = u_hist[StructProps.DIST_ARRAY]
-    #                 u_emd = emd_pyphi(*u_dist, *self._target_dist)
-    #                 ic(u_emd)
-
-    #                 print()
-
-    #                 # Ciclos asociados a omega
-    #                 idx_o_actual = {bin: [] for bin in BOOL_RANGE}
-    #                 idx_o_effect = {bin: [] for bin in BOOL_RANGE}
-    #                 o_concept = (idx_o_actual, idx_o_effect)
-
-    #                 o_com = t_com - betha
-    #                 ic(betha)
-    #                 ic(o_com)
-
-    #                 for p, q in betha:
-    #                     o_concept[1 - p][rep_one].append(q)
-
-    #                 for i, j in o_com:
-    #                     o_concept[1 - i][rep_two].append(j)
-
-    #                 o_effect, o_actual = o_concept
-    #                 ic(o_effect)
-    #                 ic(o_actual)
-
-    #                 # Creación de distribuciones omega
-    #                 omega_hist = omega_dist.create_distrib(o_effect, o_actual, data=True)
-    #                 omega_dist = omega_hist[StructProps.DIST_ARRAY]
-    #                 o_emd = emd_pyphi(*omega_dist, *self._target_dist)
-    #                 ic(o_emd)
-
-    #                 print(f'{o_emd - u_emd:.2f}')
-
-    #                 all_mips.append(Deletion(u, betha, o_emd, u_emd, o_emd - u_emd))
-
-    #             best_mip = min(all_mips, key=lambda x: x.get_emd())
-    #             omega.add(best_mip.get_edge())
-    #             ic(omega)
-    #             print('*' * 20)
-
-    #             # El mejor se añade en omega
-
-    #         break  #! Remove this break !#
-    #     return DUMMY_DELETION
 
     # """
     # ic| t: (0, 1), u: (1, 2), u_com: {(1, 0), (0, 2), (1, 1), (0, 0)}
@@ -378,27 +268,6 @@ class QRNodes(Sia):
     def effect_edge_by_index(self, index):
         return self.__effect_labels[self._effect.index(index)]
 
-    # # self.__net = self.remove_edges(self.__net, [x])
-
-    # if net.is_disconnected(trimmed_net):
-    #     # Disconexo => reestablecemos la arista, guardamos la MIP.
-    #     # self.__net.add_weighted_edges_from([(*x, subtrahend_emd)])
-    #     iter_part.append(deletion)
-
-    # elif minuend_emd > FLOAT_ZERO and subtrahend_emd > FLOAT_ZERO:
-    #     # Conexo & hay pérdida => restablecemos la arsita.
-    #     # ? self.remove_edges(self.__net, [x])
-    #     # self.__net.add_weighted_edges_from([(*x, subtrahend_emd)])
-    #     ...
-
-    # else:
-    #     # Conexo & no hay pérdida => guardamos la arista. A su vez guardamos estas aristas en 0 para la reconstrucción.
-    #     iter_dels.append(deletion)
-    #     struct.set_matrix(x[V_IDX], substruct.get_matrix(x[V_IDX]))
-
-    # self.plot_net(self.__net)
-
-    # self.plot_net(trimmed_net)
 
     def plot_net(self, net: nx.Graph) -> None:
         """This function is used to plot the network."""
